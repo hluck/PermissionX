c742f347946784be0d169ba5a5e0abf0181c43c2
刘金华 <1533761530@qq.com>
2022-12-09 14:39:14 +0800

    PermissionX

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.permissionx.luckl.PermissionX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val callBtn = findViewById<Button>(R.id.btn_call)
+        callBtn.setOnClickListener {
+            PermissionX.request(this,Manifest.permission.CALL_PHONE){ allGranted,deniedList ->
+                if (allGranted){
+                    call()
+                } else {
+                    Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call(){
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e:SecurityException){
+            e.printStackTrace()
+        }
+    }
+
+    fun requestManyPermissionExample(){
+        PermissionX.request(this,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_CONTACTS) { allGranted, deniedList ->
+            if (allGranted) {
+                Toast.makeText(this, "All permissions are granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
